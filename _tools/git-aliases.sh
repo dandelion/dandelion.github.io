abd40793cb74a8167a72a688ff4263ea830e4c8f
Romain Lespinasse <romain.lespinasse@gmail.com>
2013-07-29 13:12:51 +0200

    Hide and Show '_site' for better view of real changes

--- _tools/git-aliases.sh
+++ _tools/git-aliases.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 git config --global alias.publish-website '!git branch -D master; git checkout -b master; git filter-branch --subdirectory-filter _site/ -f; git checkout source; git push --all origin'
+git config --global alias.hide-site 'update-index --assume-unchanged _site'
+git config --global alias.show-site 'update-index --no-assume-unchanged _site'
