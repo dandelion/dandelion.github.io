14e06c50699acde624b79b903c05d4fb2b9166a3
Romain Lespinasse <romain.lespinasse@gmail.com>
2013-09-04 16:32:04 +0200

    upgrade git aliases for stronger publish-website
    Now the alias, build website with jekyll, commit all the change, and publish the website

--- _tools/git-aliases.sh
+++ _tools/git-aliases.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-git config --global alias.publish-website '!git branch -D master; git checkout -b master; git filter-branch --subdirectory-filter _site/ -f; git checkout source; git push --all origin'
-git config --global alias.hide-site 'update-index --assume-unchanged _site'
-git config --global alias.show-site 'update-index --no-assume-unchanged _site'
+git config --global alias.publish-website '!jekyll build; git add -A *; git commit -m "build dandelion website by jekyll"; git branch -D master; git checkout -b master; git filter-branch --subdirectory-filter _site/ -f; git push origin master --force; git checkout source'
